acb22304cd2bd101a7afcb61f7118397245b88c3
yabsayele <yabiayele2023@u.northwestern.edu>
2022-04-05 18:22:37 -0500

    filter by time

--- src/App.js
+++ src/App.js
@@ -3,6 +3,33 @@ import Banner from './components/Banner';
 import CourseList from './components/CourseList';
 import './App.css';
 
+const meetsPat = /^ *((?:M|Tu|W|Th|F)+) +(\d\d?):(\d\d) *[ -] *(\d\d?):(\d\d) *$/;
+
+const timeParts = meets => {
+  const [match, days, hh1, mm1, hh2, mm2] = meetsPat.exec(meets) || [];
+  return !match ? {} : {
+    days,
+    hours: {
+      start: hh1 * 60 + mm1 * 1,
+      end: hh2 * 60 + mm2 * 1
+    }
+  };
+};
+
+const mapValues = (fn, obj) => (
+  Object.fromEntries(Object.entries(obj).map(([key, value]) => [key, fn(value)]))
+);
+
+const addCourseTimes = course => ({
+  ...course,
+  ...timeParts(course.meets)
+});
+
+const addScheduleTimes = schedule => ({
+  title: schedule.title,
+  courses: mapValues(addCourseTimes, schedule.courses)
+});
+
 const App = () => {
   const [schedule, setSchedule] = useState();
   const url = 'https://courses.cs.northwestern.edu/394/data/cs-courses.php';
@@ -12,7 +39,7 @@ const App = () => {
       const response = await fetch(url);
       if (!response.ok) throw response;
       const json = await response.json();
-      setSchedule(json);
+      setSchedule(addScheduleTimes(json));
     }
     fetchSchedule();
   }, []);
--- src/components/CourseList.js
+++ src/components/CourseList.js
@@ -10,14 +10,51 @@ const getCourseNumber = course => (
   course.id.slice(1, 4)
 );
 
-const Course = ({ course }) => (
-  <div className="card m-1 p-2">
-    <div className="card-body">
-      <div className="card-title">{ getCourseTerm(course) } CS { getCourseNumber(course) }</div>
-      <div className="card-text">{ course.title }</div>
-    </div>
-  </div>
+const toggle = (x, lst) => (
+  lst.includes(x) ? lst.filter(y => y !== x) : [x, ...lst]
 );
+
+const days = ['M', 'Tu', 'W', 'Th', 'F'];
+
+const daysOverlap = (days1, days2) => ( 
+  days.some(day => days1.includes(day) && days2.includes(day))
+);
+
+const hoursOverlap = (hours1, hours2) => (
+  Math.max(hours1.start, hours2.start) < Math.min(hours1.end, hours2.end)
+);
+
+const timeConflict = (course1, course2) => (
+  daysOverlap(course1.days, course2.days) && hoursOverlap(course1.hours, course2.hours)
+);
+
+const courseConflict = (course1, course2) => (
+  getCourseTerm(course1) === getCourseTerm(course2)
+  && timeConflict(course1, course2)
+);
+
+const hasConflict = (course, selected) => (
+  selected.some(selection => courseConflict(course, selection))
+);
+
+const Course = ({ course, selected, setSelected }) => {
+  const isSelected = selected.includes(course);
+  const isDisabled = !isSelected && hasConflict(course, selected);
+  const style = {
+    backgroundColor: isDisabled? 'lightgray' : isSelected ? 'lightgreen' : 'white'
+  };
+  return (
+    <div className="card m-1 p-2"
+    onClick={ isDisabled ? null : () => setSelected(toggle(course, selected))}
+    style={style}>
+      <div className="card-body">
+        <div className="card-title">{ getCourseTerm(course) } CS { getCourseNumber(course) }</div>
+        <div className="card-text">{ course.title }</div>
+        <div className="card-text">{ course.meets }</div>
+      </div>
+    </div>
+  );
+};
 
 const TermButton = ({term, setTerm, checked}) => (
   <>
@@ -41,13 +78,19 @@ const TermSelector = ({term, setTerm}) => (
 
 const CourseList = ({ courses }) => {
   const [term, setTerm] = useState('Fall');
+  const [selected, setSelected] = useState([]);
   const termCourses = Object.values(courses).filter(course => term === getCourseTerm(course));
   
   return (
     <>
       <TermSelector term={term} setTerm={setTerm}/>
       <div className="course-list">
-      { termCourses.map(course => <Course key={course.id} course={ course } />) }
+      { 
+        termCourses.map(course => 
+          <Course key={course.id} course={ course }
+            selected={selected} setSelected={ setSelected } 
+          />) 
+      }
       </div>
     </>
   );
